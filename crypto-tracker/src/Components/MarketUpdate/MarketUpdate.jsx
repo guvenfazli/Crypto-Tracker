142a5481c8d6ae0f540fbea4b2de16b9caaaab48
Güven Fazlı <guven.fazli@gmail.com>
2024-04-10 02:36:56 +0300

    Detail Section Responsive is done

--- crypto-tracker/src/Components/MarketUpdate/MarketUpdate.jsx
+++ crypto-tracker/src/Components/MarketUpdate/MarketUpdate.jsx
@@ -6,11 +6,34 @@ import { useState } from 'react';
 
 export default function MarketUpdate({ data }) {
   const [details, setDetails] = useState()
-  const limitedData = data.slice(0, 10)
+  const [controlTable, setControlTable] = useState({
+    start: 0,
+    end: 10
+  })
+
+  const limitedData = data.slice(controlTable.start, controlTable.end)
   console.log(limitedData)
 
   function renderDetails(coin) {
     setDetails(coin)
+  }
+
+  function navigateTable(option){
+    if(option === "+"){
+      setControlTable((prev) => {
+        let newPage = {...prev}
+        newPage.start += 10
+        newPage.end += 10
+        return newPage
+      }) 
+    } else {
+      setControlTable((prev) => {
+        let newPage = {...prev}
+        newPage.start -= 10
+        newPage.end -= 10
+        return newPage
+      }) 
+    }
   }
 
 
@@ -37,32 +60,38 @@ export default function MarketUpdate({ data }) {
 
 
       {details ? <SmallChart detail={setDetails} data={details} /> :
-        <table>
+        <>
+          <table>
 
-          <thead>
-            <tr>
-              <td>NO</td>
-              <td>NAME</td>
-              <td>LAST PRICE</td>
-              <td>CHANGE</td>
-              <td>TRADE</td>
-            </tr>
-          </thead>
+            <thead>
+              <tr>
+                <td>NO</td>
+                <td>NAME</td>
+                <td>LAST PRICE</td>
+                <td>CHANGE</td>
+                <td>TRADE</td>
+              </tr>
+            </thead>
 
-          <tbody>
+            <tbody>
 
-            {limitedData.map((coin, index) =>
-              <tr key={coin.id}>
-                <td data-cell="NO">{index + 1}</td>
-                <td data-cell="NAME" className={classes.coinInfo}><div className={classes.coinBg} style={{ backgroundImage: `url(${coin.image})` }} /> {coin.name}</td>
-                <td data-cell="PRICE">{coin.current_price.toFixed(1)} $</td>
-                <td data-cell="CHANGE">{coin.price_change_percentage_24h} %</td>
-                <td data-cell="TRADE"><button onClick={() => renderDetails(coin)}>See Details</button></td>
-              </tr>)}
+              {limitedData.map((coin, index) =>
+                <tr key={coin.id}>
+                  <td data-cell="NO">{index + 1}</td>
+                  <td data-cell="NAME" className={classes.coinInfo}><div className={classes.coinBg} style={{ backgroundImage: `url(${coin.image})` }} /> {coin.name}</td>
+                  <td data-cell="PRICE">{coin.current_price.toFixed(1)} $</td>
+                  <td data-cell="CHANGE">{coin.price_change_percentage_24h} %</td>
+                  <td data-cell="TRADE"><button onClick={() => renderDetails(coin)}>See Details</button></td>
+                </tr>)}
 
-          </tbody>
+            </tbody>
 
-        </table>
+          </table>
+          <div className={classes.tablePage}>
+            <button disabled={controlTable.start <= 0} onClick={() => navigateTable("-")}>Previous</button>
+            <button disabled={controlTable.end >= data.length} onClick={() => navigateTable("+")}>Next</button>
+          </div>
+        </>
 
       }
 
--- crypto-tracker/src/Components/MarketUpdate/SmallChart/SmallChart.jsx
+++ crypto-tracker/src/Components/MarketUpdate/SmallChart/SmallChart.jsx
@@ -1,8 +1,10 @@
-import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, domain, Label } from 'recharts';
+import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, domain, Label, Tooltip } from 'recharts';
 import classes from "./smallchart.module.css"
 /* U T I L S */
 import { fetchCoinData } from '../../../Utils/fetchCoins';
 import { useEffect, useState } from 'react';
+
+import { backArrow } from './smallcharticon';
 
 export default function SmallChart({ data, detail }) {
 
@@ -10,10 +12,10 @@ export default function SmallChart({ data, detail }) {
 
   useEffect(() => {
     async function coinData() {
-      const priceHistory = await fetchCoinData(data.id)
-      const limitedHistory = priceHistory.prices.slice(0, 288)
+      const priceData = await fetchCoinData(data.id)
+      const priceHistory = priceData.prices
       setCoinData(() => {
-        let prices = limitedHistory.map((price) => { return { value: price[1].toFixed(2) } })
+        let prices = priceHistory.map((price) => { return { value: price[1].toFixed(2) } })
         let priceList = prices
         return priceList
       })
@@ -24,7 +26,7 @@ export default function SmallChart({ data, detail }) {
 
   console.log(coinData)
 
-  function goBack(){
+  function goBack() {
     detail(undefined)
   }
 
@@ -32,34 +34,46 @@ export default function SmallChart({ data, detail }) {
 
   return (
     <div className={classes.details}>
-      <button onClick={goBack}>Back</button>
-      <div className={classes.coinIcon} style={{backgroundImage: `url(${data.image})`}}/>
-      <div className={classes.coinInfo}>
-        <div>
-          <p className={classes.title}>{data.name}</p>
-          <p>Current Price: {data.current_price} $</p>
-        </div>
+      <div className={classes.detailNav}>
+        <button onClick={goBack}>{backArrow}</button>
 
-        <div>
-          <p>Price Change as Percentage in 24h: {data.price_change_percentage_24h.toFixed(1)} %</p>
-          <p>Price Change in 24h: {data.price_change_24h.toFixed(1)} %</p>
+      </div>
+      <div className={classes.coinInfo}>
+
+        <div className={classes.coinIcon} style={{ backgroundImage: `url(${data.image})` }} />
+        <div className={classes.coinStats}>
+          <div>
+            <p className={classes.title}>{data.name}</p>
+            <p>Current Price: <span style={{ color: "#B6B6B6" }}>{data.current_price} $</span></p>
+            <p>Price Change as Percentage in 24h: <span style={{ color: data.price_change_percentage_24h <= 0 ? '#D32F2F' : '#4CAF50', fontWeight: "bold" }}>{data.price_change_percentage_24h.toFixed(1)} %</span></p>
+            <p>Price Change in 24h: <span style={{ color: data.price_change_24h <= 0 ? '#D32F2F' : '#4CAF50', fontWeight: "bold" }}>{data.price_change_24h.toFixed(1)} $</span></p>
+
+          </div>
+
+          <div>
+            <p>All Time High: <span style={{ color: "#B6B6B6" }}>{data.ath} $</span></p>
+            <p>All Time Low: <span style={{ color: "#B6B6B6" }}>{data.atl} $</span></p>
+            <p>Highest in 24h: <span style={{ color: "#B6B6B6" }}>{data.high_24h} $</span></p>
+            <p>Lowest in 24h: <span style={{ color: "#B6B6B6" }}>{data.low_24h} $</span></p>
+          </div>
+
         </div>
 
       </div>
 
-      <div>Price Change in 24h in Graphics</div>
+      <div className={classes.tableTitle} style={{ display: "flex", justifyContent: "center", marginBottom: "5rem" }}>Price Change in 24h in Graphics</div>
 
 
 
 
-      <ResponsiveContainer  width="100%" height={700}>
-        <LineChart data={coinData}>
-          <Line strokeWidth={2.5} stroke={data.price_change_24h > 0 ? '#4CAF50' : '#D32F2F'} dot={false} dataKey="value" />
-          <XAxis tick={false}/>
-          <YAxis style={{fontSize: "0.7rem", padding: "0"}}  domain={['dataMin', 'dataMax']} values='value'/>
-          <CartesianGrid  stroke='#555773'/>
+      <ResponsiveContainer width="100%" height={500}>
+        <LineChart data={coinData} margin={{right: 25}}>
+          <Line strokeWidth={2.5} type="monotone" activeDot={{ r: 2 }} stroke={data.price_change_24h > 0 ? '#4CAF50' : '#D32F2F'} dot={false} dataKey="value" />
+          <XAxis tick={false} />
+          <Tooltip itemStyle={{color: "black"}} contentStyle={{backgroundColor: "rgb(255,255,255,0.3)", border: "none", borderRadius: "1rem"}} labelStyle={{display:'none'}}/>
+          <YAxis style={{ fontSize: "0.7rem", padding: "0" }} domain={['dataMin', 'dataMax']} values='value' />
+          <CartesianGrid stroke='#555773' />
         </LineChart>
-        
       </ResponsiveContainer>
     </div>
   )
